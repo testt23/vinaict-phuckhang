8b0e6d7eae7cdff02bfcacb49aeb407a2c458405
lehoangchanh.uit <lehoangchanh.uit@gmail.com>
2012-06-19 04:12:04 +0000

    task: ... status: ...

---
+++ lhchanh/language/vi/err_lang.php
@@ -0,0 +1,96 @@
+<?php
+
+/* User */
+
+$lang['err_empty_name']                     = 'Bạn chưa nhập tên';
+$lang['err_invalid_name']                   = 'Tên nhập vào không hợp lệ';
+$lang['err_name_wrong_format']              = 'Tên nhập vào bị sai định dạng';
+$lang['err_empty_first_name']               = 'Bạn chưa nhập tên';
+$lang['err_empty_last_name']                = 'Bạn chưa nhập họ';
+$lang['err_name_too_long']                  = 'Tên nhập vào quá dài';
+$lang['err_first_name_too_long']            = 'Tên nhập vào quá dài';
+$lang['err_last_name_too_long']             = 'Họ nhập vào quá dài';
+$lang['err_empty_email']                    = 'Bạn chưa nhập email';
+$lang['err_invalid_email']                  = 'Email không hợp lệ';
+$lang['err_email_too_long']                 = 'Email nhập vào quá dài';
+$lang['err_email_exists']                   = 'Địa chỉ email này đã được sử dụng';
+$lang['err_empty_password']                 = 'Bạn chưa nhập mật khẩu';
+$lang['err_empty_new_password']             = 'Bạn chưa nhập mật khẩu mới';
+$lang['err_password_too_long']              = 'Mật khẩu nhập vào quá dài';
+$lang['err_password_too_short']             = 'Mật khẩu nhập vào quá ngắn';
+$lang['err_empty_confirm_password']         = 'Bạn chưa xác nhận mật khẩu';
+$lang['err_password_confirm_not_match']     = 'Mật khẩu xác nhận không khớp';
+
+
+/* Menu */
+
+$lang['err_empty_menu_name']                = 'Bạn chưa nhập Tên Menu';
+$lang['err_empty_uri']                      = 'Bạn chưa nhập URI';
+$lang['err_empty_position']                 = 'Bạn chưa nhập vị trí menu';
+$lang['err_invalid_position']               = 'Vị trí của menu không hợp lệ (Bạn chỉ được nhập số nguyên > 0)';
+$lang['err_position_too_big']               = 'Vị trí nhập vào quá lớn';
+$lang['err_max_position_is']                = 'Vị trí lớn nhất hiện tại là';
+$lang['err_max_allowed_position_is']        = 'Bạn chỉ được nhập số lớn nhất là';
+$lang['err_empty_section']                  = 'Bạn chưa nhập Section';
+$lang['err_section_too_long']               = 'Section nhập vào quá dài';
+$lang['err_email_deactivated']              = 'Tài khoản người dùng này chưa được kích hoạt hoặc đã bị khóa';
+$lang['err_empty_email_or_password']        = 'Xin vui lòng nhập đầy đủ email và mật khẩu';
+$lang['err_email_not_exist']                = 'Địa chỉ email này không tồn tại';
+$lang['err_wrong_email_or_password']        = 'Email hoặc mật khẩu đăng nhập sai';
+
+/* Page */
+
+$lang['err_empty_title']                    = 'Bạn chưa nhập tiêu đề';
+$lang['err_empty_content']                  = 'Bạn chưa nhập nội dung';
+$lang['err_title_wrong_format']             = 'Tiêu đề nhập vào sai định dạng';
+$lang['err_content_wrong_format']           = 'Nội dung nhập vào sai định dạng';
+$lang['err_invalid_title']                  = 'Tiêu đề không hợp lệ';
+$lang['err_invalid_content']                = 'Nội dung không hợp lệ';
+$lang['err_empty_url']                      = 'Bạn chưa nhập URL';
+$lang['err_invalid_url']                    = 'URL không hợp lệ';
+$lang['err_title_too_long']                 = 'Tiêu đề quá dài';
+$lang['err_url_too_long']                   = 'URL quá dài';
+
+/* Image */
+$lang['err_invalid_picture']                = 'File hình ảnh không hợp lệ';
+$lang['err_wrong_file_dimension']           = 'Kích thước file hình ảnh lớn hơn kích thước tối đa cho phép';
+$lang['err_wrong_file_size']                = 'Xin vui lòng chỉ upload file dưới 4Mb';
+$lang['err_wrong_file_type']                = 'Xin vui lòng chỉ upload file JPG, PNG, GIF, JPEG';
+
+/* Product */
+$lang['err_empty_product_code']             = 'Bạn chưa nhập mã sản phẩm';
+$lang['err_empty_product_name']             = 'Bạn chưa nhập tên sản phẩm';
+$lang['err_invalid_product_name']           = 'Tên sản phẩm không hợp lệ';
+$lang['err_invalid_product_short_desc']     = 'Mô tả vắn tắt không hợp lệ';
+$lang['err_invalid_product_desc']           = 'Mô tả không hợp lệ';
+$lang['err_product_name_too_long']          = 'Tên sản phẩm quá dài';
+$lang['err_short_desc_too_long']            = 'Nội dung mô tả vắn tắt quá dài';
+$lang['err_empty_product_link']             = 'Bạn chưa nhập liên kết';
+$lang['err_invalid_product_link']           = 'Liên kết không hợp lệ';
+$lang['err_empty_product_category']         = 'Bạn chưa chọn nhóm sản phẩm';
+$lang['err_empty_primary_product_category'] = 'Bạn chưa chọn nhóm sản phẩm chính';
+$lang['err_product_code_exists']            = 'Đã tồn tại sản phẩm này';
+
+/* Product category */
+$lang['err_empty_product_category_code']    = 'Bạn chưa nhập mã nhóm sản phẩm';
+$lang['err_product_category_code_exists']   = 'Nhóm sản phẩm này đã tồn tại';
+$lang['err_empty_product_category_name']    = 'Bạn chưa nhập tên nhóm sản phẩm';
+$lang['err_invalid_product_category_name']  = 'Tên nhóm sản phẩm không hợp lệ';
+$lang['err_invalid_product_category_desc']  = 'Thông tin mô tả nhóm sản phẩm không hợp lệ';
+$lang['err_empty_product_category_link']    = 'Bạn chưa nhập liên kết cho nhóm sản phẩm này';
+$lNG['err_invalid_product_category_link']   = 'Liên kế nhóm sản phẩm không hợp lệ';
+
+/* Customer */
+$lang['err_empty_company_name']             = 'Bạn chưa nhập tên công ty hoặc tổ chức';
+$lang['err_empty_tax_code']                 = 'Bạn chưa nhập mã số thuế';
+$lang['err_empty_tax_code']                 = 'Bạn chưa nhập mã số thuế';
+$lang['err_empty_gender']                   = 'Bạn chưa chọn giới tính';
+$lang['err_empty_contact_address']          = 'Bạn chưa nhập địa chỉ liên hệ';
+$lang['err_empty_billing_address']          = 'Bạn chưa nhập địa chỉ thanh toán';
+$lang['err_empty_shipping_address']         = 'Bạn chưa nhập địa chỉ nhận hàng';
+$lang['err_invalid_home_phone']             = 'Số điện thoại nhà không hợp lệ';
+$lang['err_invalid_work_phone']             = 'Số điện thoại công ty không hợp lệ';
+$lang['err_invalid_mobile_phone']           = 'Số điện thoại di động không hợp lệ';
+$lang['err_invalid_fax']                    = 'Số fax không hợp lệ';
+$lang['err_empty_birthdate']                = 'Bạn chưa nhập ngày sinh';
+$lang['err_empty_contact_person']           = 'Bạn chưa nhập tên người liên hệ';
---
+++ lhchanh/views/category/insert.php
@@ -0,0 +1,14 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+//echo '<br>' . $sql;
+?>
+
+<form action="" method="post">
+    Name: <input type="text" name="name"><br >
+    <input type="submit" value="OK">
+</form>
