95fba74cffd23c68100ec8e30d68f1b43a690feb
truong.ly@gmail.com <truong.ly@gmail.com@8ef986e8-94b3-981c-ca0d-1d3ddbd753bb>
2012-06-21 16:35:02 +0000

    Removed branches/v1.1/thehalfheart

--- branches/v1.1/thehalfheart/config/pagination.php
+++
@@ -1,9 +0,0 @@
-<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-
-$config['num_links'] = "4";
-$config['use_page_numbers'] = TRUE;
-$config['page_query_string'] = TRUE;
-$config['query_string_segment'] = 'page';
-
-/* End of file pagination.php */
-/* Location: ./system/application/config/pagination.php */
