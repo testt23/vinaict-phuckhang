9d1da27bc3ba0f5e7cd44af322aa60bfea3dfb44
truong.ly <truong.ly@gmail.com>
2012-06-21 16:35:41 +0000

    Removed branches/v1.1/lhchanh

--- lhchanh/views/category/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-    while ($category->fetchNext()){   
-        echo $category->id . ' - ' . $category->name . '<br>' ;
-    }
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-?>
--- lhchanh/views/product/product.php
+++
@@ -1,1 +0,0 @@
-<h1><?php echo __FILE__ ;?></h1>
