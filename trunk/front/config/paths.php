f7d12251c3629b6660280da178a14ed999ed0b76
truong.ly@gmail.com <truong.ly@gmail.com@8ef986e8-94b3-981c-ca0d-1d3ddbd753bb>
2012-06-22 10:20:03 +0000

    Fix path to dynamically

--- trunk/front/config/paths.php
+++ trunk/front/config/paths.php
@@ -1,6 +1,6 @@
 <?php
-
-define('URL', 'http://localhost/SMART/phuckhang/trunk/front/');
+define('PROTOCOL', isset($_SERVER['HTTPS']) ? 'https' : 'http');
+define('URL', PROTOCOL.'://'.$_SERVER['SERVER_NAME'].$_SERVER['REQUEST_URI']);
 define('VIEW_PATH', URL . 'views/');
 define('PUBLIC_PATH', URL . 'public/');
 define('IMAGE_PATH', PUBLIC_PATH . 'images/');
