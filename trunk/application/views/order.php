7e755ffa6edbd1519f6a5188f760d35ee7a7f6c3
cohacone.nam@gmail.com <cohacone.nam@gmail.com@8ef986e8-94b3-981c-ca0d-1d3ddbd753bb>
2012-07-06 16:17:05 +0000

    Manager purchase_order: add field currency in table purchase_order

--- trunk/application/views/order.php
+++ trunk/application/views/order.php
@@ -90,8 +90,8 @@ while ($order_status->fetchNext()) {
                         ?>
                     </td>
                     <td><?php echo $order->description; ?></td>
-                    <td style="text-align: center;"><?php  //echo date_sql_to_local_date($order->order_date, $local_timezone, true, false); ?></td>
-                    <td style="text-align: center;"><?php  //echo date_sql_to_local_date($order->shipping_date, $local_timezone, true, false); ?></td>
+                    <td style="text-align: center;"><?php  echo date_sql_to_local_date($order->order_date, $local_timezone, true, false); ?></td>
+                    <td style="text-align: center;"><?php  echo date_sql_to_local_date($order->shipping_date, $local_timezone, true, false); ?></td>
                     <td style="text-align: right;"><?php echo $order->amount ? floatval($order->amount).' '.$order->currency : lang('txt_call'); ?></td>
                     <td style="text-align: center;">
                         <?php
