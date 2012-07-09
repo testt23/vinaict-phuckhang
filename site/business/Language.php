11879cd7313f5bd04dc55e2850dc45bf712ee8e6
cohacone.nam <cohacone.nam@gmail.com>
2012-07-09 11:15:39 +0000

    change language on frontend

--- site/business/Language.php
+++ site/business/Language.php
@@ -5,4 +5,21 @@
 		function __construct() {
 			parent::__construct();
 		}
+                
+                function getList() {
+                    $lang = new Language();
+                    $lang->addSelect();
+                    $lang->addSelect('*');
+                    $lang->find();
+                    return $lang;
+                }
+                
+                function getArrayLangIso() {
+                    $lang = self::getList();
+                    $arr_lang = array();
+                    while($lang->fetchNext()) {
+                        $arr_lang[] = $lang->code;
+                    }
+                    return $arr_lang;
+                }
 	}
--- site/controllers/language_controller.php
+++ site/controllers/language_controller.php
@@ -5,7 +5,10 @@ class Language_controller extends CI_Controller{
     function __construct() {
         parent::__construct();
     }
-    public function index($lang){
+    public function index(){
+        
+        $lang = $this->input->get_post('lang');
+        
         $this->session->set_userdata('lang', $lang);
         redirect($this->session->userdata('lang_url'));
     }
