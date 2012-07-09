7df3249bbba51f93d386cf216c5d2fda2404fc92
huynhbaan <huynhbaan@gmail.com>
2012-07-09 08:16:21 +0000

    Managerment: Page Front End - site - LANG

--- site/language/en/site_lang.php
+++ site/language/en/site_lang.php
@@ -7,6 +7,7 @@ $lang['site_footer_copy_right']                 = '© Copyright 2012 NoiThatDatV
 $lang['site_product_order'] = 'order';
 
 $lang['site_product_title_order'] = 'Your Order';
+$lang['site_product_order'] = 'Order';
 $lang['site_product_name'] = 'Product name';
 $lang['site_product_code'] = 'Product code';
 $lang['site_product_price'] = 'Product price';
@@ -14,3 +15,52 @@ $lang['site_product_number'] = 'Number';
 $lang['site_product_delete'] = 'Delete';
 
 $lang['site_product_continue'] = 'Continue';
+$lang['lbl_price'] = 'Price: ';
+$lang['lbl_description'] = 'Description: ';
+$lang['show_message'] = 'You do not have any products in your shopping cart';
+
+// contact form
+
+$lang['show_empty_email'] = 'Please enter a valid email address.';
+$lang['show_format_email'] = 'Email format is incorrect.';
+$lang['show_empty_phone'] = 'Please enter a valid phone number.';
+$lang['show_empty_billing'] = 'Please enter a valid billing.';
+$lang['show_empty_shipping'] = 'Please enter a valid shipping.';
+$lang['show_empty_company'] = 'Please enter a valid company.';
+$lang['show_empty_personal'] = 'Please enter a valid personal.';
+$lang['show_empty_firstname'] = 'Please enter a valid firstname.';
+$lang['show_empty_lastname'] = 'Please enter a valid lastname.';
+$lang['show_empty_message'] = 'Please enter a valid content message.';
+
+$lang['note_form'] = 'Please type your info below';
+$lang['note_content'] = 'If you ever purchase, you can import email and empty the remaining cells, if you want to change any information, then enter that information';
+$lang['lbl_purpose'] = 'Purpose:';
+$lang['lbl_buy_by'] = 'Buy by:';
+$lang['rdb_no_purchases'] = 'No purchases';
+$lang['rdb_having_a_purchase'] = 'Having a purchases';
+$lang['lbl_phone'] = 'Phone:';
+$lang['lbl_billing_address'] = 'Billing Address:';
+$lang['lbl_shipping_address'] = 'Shipping Address:';
+$lang['lbl_company'] = 'Company:';
+$lang['lbl_contact_with'] = 'Contact with:';
+$lang['lbl_home_address'] = 'Home address:';
+$lang['lbl_website_address'] = 'Website:';
+$lang['lbl_tax_code'] = 'Tax code:';
+$lang['lbl_email_address'] = 'Email:';
+$lang['lbl_yahoo_address'] = 'Yahoo:';
+$lang['lbl_skype_address'] = 'Skype:';
+$lang['btn_clear'] = 'CLEAR';
+$lang['btn_order'] = 'ORDER';
+$lang['rdb_gender_mr'] = 'Mr';
+$lang['rdb_gender_ms'] = 'Ms';
+
+$lang['lbl_firstname'] = 'Firstname:';
+$lang['lbl_lastname'] = 'Lastname:';
+$lang['lbl_career'] = 'Career:';
+$lang['cobx_contact'] = 'Contact';
+$lang['cobx_buy'] = 'Buy';
+$lang['cobx_for_company'] = 'Company';
+$lang['cobx_for_personal'] = 'Personal';
+
+// page product detail
+$lang['show_message_product'] = 'No product updates';
