638eea30b3c3dfaf4a7ef0280f5b04a09eb1a513
huynhbaan@gmail.com <huynhbaan@gmail.com@8ef986e8-94b3-981c-ca0d-1d3ddbd753bb>
2012-07-09 08:16:21 +0000

    Managerment: Page Front End - site - LANG

--- trunk/site/language/vi/site_lang.php
+++ trunk/site/language/vi/site_lang.php
@@ -17,5 +17,55 @@ $lang['site_product_delete'] = 'Xóa';
 
 
 $lang['site_product_continue'] = 'Tiếp tục mua';
+$lang['lbl_price'] = 'Giá: ';
+$lang['lbl_description'] = 'Mô tả: ';
+$lang['show_message'] = 'Bạn chưa có sản phẩm nào trong giỏ hàng';
 
+// contact form
+
+$lang['show_empty_email'] = 'Vui lòng nhập vào địa chỉ Email.';
+$lang['show_format_email'] = 'Email không đúng định dạng.';
+$lang['show_empty_phone'] = 'Vui lòng nhập vào số điện thoại.';
+$lang['show_empty_billing'] = 'Vui lòng nhập vào địa chỉ thanh toán.';
+$lang['show_empty_shipping'] = 'Vui lòng nhập vào địa chỉ giao hàng.';
+$lang['show_empty_company'] = 'Vui lòng nhập vào tên công ty.';
+$lang['show_empty_personal'] = 'Vui lòng nhập vào người đại diện công ty.';
+$lang['show_empty_firstname'] = 'Vui lòng nhập vào đầy đủ Họ và Tên.';
+$lang['show_empty_lastname'] = 'Vui lòng nhập vào địa chỉ Email.';
+$lang['show_empty_message'] = 'Vui lòng nhập vào nội dung tin nhắn.';
+
+
+$lang['note_form'] = 'Khách hàng vui lòng nhập thông tin của mình vào khung bên dưới ';
+$lang['note_content'] = 'Nếu bạn đã từng mua hàng thì bạn có thể nhập email và để trống các ô còn lại, nếu bạn muốn thay đổi thông tin nào thì nhập vào thông tin đó';
+$lang['lbl_purpose'] = 'Mục đích:';
+$lang['lbl_buy_by'] = 'Mua bởi:';
+$lang['rdb_no_purchases'] = 'Chưa mua hàng';
+$lang['rdb_having_a_purchase'] = 'Đã từng mua hàng';
+$lang['lbl_phone'] = 'Số điện thoại:';
+$lang['lbl_billing_address'] = 'Địa chỉ thanh toán:';
+$lang['lbl_shipping_address'] = 'Địa chỉ giao hàng:';
+$lang['lbl_company'] = 'Công ty:';
+$lang['lbl_contact_with'] = 'Người liên hệ:';
+$lang['lbl_home_address'] = 'Địa chỉ nhà riêng:';
+$lang['lbl_website_address'] = 'Địa chỉ Website:';
+$lang['lbl_tax_code'] = 'Mã số thuế:';
+$lang['lbl_email_address'] = 'Địa chỉ Email:';
+$lang['lbl_yahoo_address'] = 'Địa chỉ Yahoo:';
+$lang['lbl_skype_address'] = 'Địa chỉ Skype:';
+$lang['btn_clear'] = 'XÓA';
+$lang['btn_order'] = 'ĐẶT HÀNG';
+$lang['rdb_gender_mr'] = 'Ông';
+$lang['rdb_gender_ms'] = 'Bà';
+
+$lang['lbl_firstname'] = 'Họ:';
+$lang['lbl_lastname'] = 'Tên:';
+$lang['lbl_career'] = 'Nghề nghiệp:';
+$lang['cobx_contact'] = 'Liên hệ';
+$lang['cobx_buy'] = 'Mua hàng';
+$lang['cobx_for_company'] = 'Công ty';
+$lang['cobx_for_personal'] = 'Cá nhân';
+
+
+// page product detail
+$lang['show_message_product'] = 'Chưa cập nhật sản phẩm';
 
