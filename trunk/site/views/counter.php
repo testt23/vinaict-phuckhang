0b2ae1d8dec696295f00d32be5e5058af02db007
cohacone.nam@gmail.com <cohacone.nam@gmail.com@8ef986e8-94b3-981c-ca0d-1d3ddbd753bb>
2012-07-12 10:04:58 +0000

    Counter

---
+++ trunk/site/views/counter.php
@@ -0,0 +1,13 @@
+<div>
+    <?php 
+        $count = Counter::add();
+        $string = '';
+        $string = $count;
+    ?>
+    <ul class ="counter">
+        <?php for($i=0; $i<strlen($string); $i++){ ?>
+        <li><?php echo $string[$i]; ?></li>
+        <?php } ?>
+    </ul>
+    
+</div>
