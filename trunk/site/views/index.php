dc75062c8eeffd5f1e933803362201d63bd4ddd3
truong.ly@gmail.com <truong.ly@gmail.com@8ef986e8-94b3-981c-ca0d-1d3ddbd753bb>
2012-07-20 13:47:59 +0000

    Product Gallery

--- trunk/site/views/index.php
+++ trunk/site/views/index.php
@@ -25,17 +25,23 @@
                         </div>
                     </div>
                     <div class="name-pro">
-                        <h3><a href="<?php echo $product->the_product_link(); ?>"><?php echo Variable::cut_string($product->the_product_name(), Variable::getNumberOfProductTitle()); ?></a></h3>
-                        <strong><?php echo lang('lbl_price'); ?></strong><span style="color: #b41500;"> 
-                            <?php
-                            if ($product->the_product_price() == '' || $product->the_product_price() == '0') {
-                                echo lang('lbl_call');
-                            } else {
-                                echo $product->the_product_price() . ' ' . $product->the_product_currency();
-                            }
-                            ?> 
-                        </span>
-
+                        <h3><a href="<?php echo $product->the_product_link(); ?>"><?php echo truncateString($product->the_product_name(), 30); ?></a></h3>
+                        <div class="info">
+                            <span class="label"><?php echo lang('lbl_prod_code').': '; ?></span>
+                            <span class="value"><?php echo $product->the_product_code(); ?></span>
+                        </div>
+                        <div class="info">
+                            <span class="label"><?php echo lang('lbl_price'); ?></span>
+                            <span class="value">
+                                <?php
+                                if ($product->the_product_price() == '' || $product->the_product_price() == '0') {
+                                    echo lang('lbl_call');
+                                } else {
+                                    echo $product->the_product_price() . ' ' . $product->the_product_currency();
+                                }
+                                ?> 
+                            </span>
+                        </div>
                     </div>
 
                 </li>     
