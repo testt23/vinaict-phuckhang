4820c5bb40cb6c3db088e3d298bb521069a5c8a6
t <thehalfheart@gmail.com>
2012-07-26 09:21:40 +0000

    update news

--- site/business/Article.php
+++ site/business/Article.php
@@ -1,35 +1,54 @@
 <?php
 
-	class Article extends Article_model {
+class Article extends Article_model {
 
-		function __construct() {
-			parent::__construct();
-		}
-                
-                 public function getList($filter = array()) {
-        
-                    $article = new Article();
+    public static $lang;
+    function __construct() {
+        parent::__construct();
+        self::$lang = get_system_language();
+    }
 
-                    $article->addJoin(new NewsCategory);
+    public function getList($filter = array()) {
 
-                    $article->addSelect();
-                    $article->addSelect('article.*, news_category.name name_category');
+        $article = new Article();
 
-                    if (isset($filter['title']) && $filter['title'])
-                        $article->addWhere("title LIKE '%".$filter['title']."%'");
+        $article->addJoin(new NewsCategory);
 
-                    if (isset($filter['keywords']) && $filter['keywords'])
-                        $article->addWhere("keywords LIKE '%".$filter['keywords']."%'");
+        $article->addSelect();
+        $article->addSelect('article.*, news_category.name name_category');
 
-                    if (isset($filter['id_news_category']) && $filter['id_news_category'])
-                        $article->addWhere("id_news_category = ".$filter['id_news_category']);
+        if (isset($filter['title']) && $filter['title'])
+            $article->addWhere("title LIKE '%" . $filter['title'] . "%'");
 
-                    $article->orderBy(getI18nRealStringSql("title"));
+        if (isset($filter['keywords']) && $filter['keywords'])
+            $article->addWhere("keywords LIKE '%" . $filter['keywords'] . "%'");
 
-                    $article->find();
+        if (isset($filter['id_news_category']) && $filter['id_news_category'])
+            $article->addWhere("id_news_category = " . $filter['id_news_category']);
 
-                    return $article;
+        $article->orderBy(getI18nRealStringSql("title"));
 
-                }
-                
-	}
+        $article->find();
+
+        return $article;
+    }
+    
+    
+    
+    public function get_id(){
+        return $this->id;
+    }
+    
+    public function get_title(){
+        return getI18n($this->title, self::$lang);
+    }
+    
+    public function get_date(){
+        return date_sql_to_local_date($this->date);
+    }
+    
+    public function get_content(){
+        return getI18n($this->id, self::$lang);
+    }
+
+}
