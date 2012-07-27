7d3ecebf2577c3024dd7e71ef9008cb270559cb7
cohacone.nam <cohacone.nam@gmail.com>
2012-07-27 15:45:30 +0000

    fix add sidebar

--- site/views/temp.php
+++ site/views/temp.php
@@ -7,7 +7,16 @@
     
     <?php $this->load->view($content); ?>
 </div>
-
-<div id="sidebar-container"></div>
+<?php if(!$selected == ''){ ?>
+<div id="sidebar-container">hjghjsadsdsads sad ds ds d asd </div>
+<?php } ?>
 <div class="clear"></div>
 <?php $this->load->view('footer'); ?>
+
+<?php if($selected == ''){ ?>
+<script >
+    $(document).ready(function(){
+        $('#content-container').css('width','100%');
+    });
+</script>
+<?php } ?>
