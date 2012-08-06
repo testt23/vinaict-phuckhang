17fc4acfc09634db2693f63a43ad4d706765bc5d
cohacone.nam@gmail.com <cohacone.nam@gmail.com@8ef986e8-94b3-981c-ca0d-1d3ddbd753bb>
2012-08-06 09:07:14 +0000

    fix css index

--- trunk/site/views/slide_show.php
+++ trunk/site/views/slide_show.php
@@ -1,8 +1,3 @@
-<script type="text/javascript">
-    $(document).ready(function(){        
-    });
-</script>
-
 <div id="slide" class="slideShowNam">
     <ul>
         <li><img src="<?php echo UPLOAD_IMAGE_URL; ?>slide_show/slide5.jpg" alt="1" /></li>      
