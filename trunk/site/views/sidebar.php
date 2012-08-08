d875e63b5522463a7b3dc4e832bbae5b01a9bd4e
thehalfheart@gmail.com <thehalfheart@gmail.com@8ef986e8-94b3-981c-ca0d-1d3ddbd753bb>
2012-08-08 09:19:53 +0000

    update sidebar shopping

--- trunk/site/views/sidebar.php
+++ trunk/site/views/sidebar.php
@@ -84,6 +84,11 @@ if ($social) {
     <div class="box-sidebar-header"><?php echo lang('txt_shopping_cart'); ?></div>
     <div class="box-sidebar-content">
         <a class="shopping-cart" href="<?php echo base_url() . 'gio-hang.html'; ?>"><img src="<?php echo base_url() . 'images/site/shopping_cart.png'; ?>" /></a>
+        <span>Bạn có: 
+            <?php 
+                echo Variable::getTotalProductShopping();
+            ?>
+        </span>
     </div>
 </div>
 
