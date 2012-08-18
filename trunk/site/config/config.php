f8b63a7a707ab2a26df2c314b450e170d2af063e
truong.ly@gmail.com <truong.ly@gmail.com@8ef986e8-94b3-981c-ca0d-1d3ddbd753bb>
2012-08-18 15:25:05 +0000

    Upload image: Add 'source_images' config item

--- trunk/site/config/config.php
+++ trunk/site/config/config.php
@@ -382,6 +382,7 @@ $config['timezone'] = 'Asia/Ho_Chi_Minh';
 $config['external_apis'] = './../external_apis/';
 
 $config['upload_path'] = '../uploads/';
+$config['source_image'] = '../uploads/images/';
 $config['allowed_types'] = 'gif|jpg|png|jpeg';
 $config['max_size'] = '4194304'; // 4MB
 $config['max_width'] = '1024';
